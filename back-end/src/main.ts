ab5772cd460cd327d3f1be0dc42d03e12ba9f1be
amrhossam902 <amrhossam902@gmail.com>
2024-05-06 15:56:33 +0300

    allow frontEnd app to connect to nest app

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    'origin': 'http://172.30.0.5:4000',
+    "methods" : "GET,HEAD,PUT,PATCH,POST,DELETE"
+  })
   await app.listen(3000);
 }
 bootstrap();
