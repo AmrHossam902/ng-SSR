28295390da3fe969762c1507dd495353fedae5c2
amrhossam902 <amrhossam902@gmail.com>
2024-05-08 01:22:41 +0300

    prepare env variables for prod deployment

--- back-end/src/db/db.module.ts
+++ back-end/src/db/db.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports:[MongooseModule.forRoot('mongodb://root:root@db-service:27017/uni-db?authSource=admin')]
+    imports:[MongooseModule.forRoot(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@db-service:${process.env.MONGO_PORT}/uni-db?authSource=admin`)]
 })
 export class DbModule {}
