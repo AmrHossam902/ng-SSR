42fb4efeee2c65b087d5aa38d99da85d73cc7c09
amrhossam902 <amrhossam902@gmail.com>
2024-05-08 03:58:46 +0300

    allow ang app to access api

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    'origin': process.env.BASE_URL,
-    "methods" : "GET,HEAD,PUT,PATCH,POST,DELETE"
+    "origin": [ process.env.BASE_URL, "http://ang-app:4000"]
   })
   await app.listen(3000);
 }
