d9d315b47163dbc863225b035c3a06e3a17b8412
amrhossam902 <amrhossam902@gmail.com>
2024-05-08 03:11:16 +0300

    test export secrets action

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    'origin': 'http://172.30.0.5:4000',
+    'origin': process.env.BASE_URL,
     "methods" : "GET,HEAD,PUT,PATCH,POST,DELETE"
   })
   await app.listen(3000);
